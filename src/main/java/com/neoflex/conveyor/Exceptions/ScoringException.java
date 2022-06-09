f3beae3f81d3030d4f10455b9b10762b8a1f1b06
Jimbanan <uservice371@mail.ru>
2022-06-09 12:54:27 +0300

    Added @ResponseStatus for ScoringException

--- src/main/java/com/neoflex/conveyor/Exceptions/ScoringException.java
+++ src/main/java/com/neoflex/conveyor/Exceptions/ScoringException.java
@@ -1,5 +1,9 @@
 package com.neoflex.conveyor.Exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class ScoringException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
--- src/main/java/com/neoflex/conveyor/controllers/ConveyorController.java
+++ src/main/java/com/neoflex/conveyor/controllers/ConveyorController.java
@@ -8,10 +8,7 @@ import com.neoflex.conveyor.services.ConveyorServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.Comparator;
