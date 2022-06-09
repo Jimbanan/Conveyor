55af20a670a6ad9fbb4f8f09a85cf5957da162ef
Jimbanan <uservice371@mail.ru>
2022-06-09 11:37:50 +0300

    Added @NotNull validation

--- src/main/java/com/neoflex/conveyor/dto/LoanApplicationRequestDTO.java
+++ src/main/java/com/neoflex/conveyor/dto/LoanApplicationRequestDTO.java
@@ -14,30 +14,39 @@ import java.time.LocalDate;
 @Schema(description = "Сущность первоначальных данных пользователя")
 public class LoanApplicationRequestDTO {
 
+    @NotNull
     @DecimalMin(value = "10000", message = "Минимальная сумма кредита: 10000")
     private BigDecimal amount;
 
+    @NotNull
     @Min(value = 6, message = "Минимальный срок кредита: 6 месяцев")
     private Integer term;
 
+    @NotNull
     @Size(min = 2, max = 30, message = "Длина имени: 2-30 символов")
     private String firstName;
 
+    @NotNull
     @Size(min = 2, max = 30, message = "Длина фамилии: 2-30 символов")
     private String lastName;
 
     @Size(min = 2, max = 30, message = "Длина отчества: 2-30 символов")
     private String middleName;
 
+    @NotNull
     @Pattern(regexp = "[\\w.]{2,50}@[\\w.]{2,20}")
     private String email;
 
+    @NotNull
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Past(message = "Дата рождения не должна быть больше текущей")
     private LocalDate birthdate;
 
+    @NotNull
     @Size(min = 4, max = 4, message = "Длина серии паспорта: 4 символа")
     private String passportSeries;
+
+    @NotNull
     @Size(min = 6, max = 6, message = "Длина номера паспорта: 6 символов")
     private String passportNumber;
 
